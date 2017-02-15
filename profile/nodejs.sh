71c9c833dcdb8282f812a60659dc07e45d258ef9
Jared Ning <jared@redningja.com>
2017-02-15 17:59:17 -0600

    Add export JARED="hi".

--- profile/nodejs.sh
+++ profile/nodejs.sh
@@ -29,6 +29,7 @@ detect_memory() {
 export PATH="$HOME/.heroku/node/bin:$HOME/.heroku/yarn/bin:$PATH:$HOME/bin:$HOME/node_modules/.bin"
 export NODE_HOME="$HOME/.heroku/node"
 export NODE_ENV=${NODE_ENV:-production}
+export JARED="hi"
 
 calculate_concurrency
 
